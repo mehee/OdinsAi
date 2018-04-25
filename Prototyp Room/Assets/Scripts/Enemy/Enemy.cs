8c8d78a0bc42b954b2753bc3033c66c63eb62d74
Unknown <wachterm89@gmail.com>
2018-04-25 15:40:41 +0200

    Monster attacking every frame

--- Prototyp Room/Assets/Scripts/Enemy/Enemy.cs
+++ Prototyp Room/Assets/Scripts/Enemy/Enemy.cs
@@ -7,14 +7,19 @@ public class Enemy : Character {
 
     private EnemyBehaviour enemyBehaviour;
     private Stats stats;
+    private float currentHealth;
     [SerializeField]
     private float attackRange;
 
     public Enemy(string name, uint currentLvl) : base(name, currentLvl)
     {
         attackRange = 2;
+        stats = new Stats();
+        setStats(currentLvl);
+        setMaxHealth(stats.getHealth() * 100);
+        currentHealth = this.getMaxHealth();
 
-        
+
     }
 
 
@@ -33,8 +38,25 @@ public class Enemy : Character {
     }
 
 
+
+
     public override void setStats(uint currentLvl)
     {
-        throw new NotImplementedException();
+        stats.setHealth(2 * (int)currentLvl);
+        stats.setArmor(1 * (int)currentLvl);
+        stats.setStrenght(2 * (int)currentLvl);
+        stats.setIntelligence(1 * (int)currentLvl);
     }
+
+
+    public void subtractHealthBy(float value)
+    {
+        this.currentHealth -= value;
+    }
+
+    public float getCurrentHealth()
+    {
+        return currentHealth;
+    }
+
 }
