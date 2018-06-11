d6f6369bcdf5f58beb4a27a65496247ecf6ff2fd
Unknown <wachterm89@gmail.com>
2018-06-11 13:18:15 +0200

    Fixed States and enemy Autoattack

--- Prototyp Room/Assets/Scripts/Characters/Enemy/EnemyStates/EvadeState.cs
+++ Prototyp Room/Assets/Scripts/Characters/Enemy/EnemyStates/EvadeState.cs
@@ -23,9 +23,13 @@ public class EvadeState : IState {
         Vector2 movementVec = (parent.MyStartPosition - parent.transform.position).normalized;
         parent.Movement.Move(movementVec);
         float distance = Vector2.Distance(parent.MyStartPosition, parent.transform.position);
-        if(distance <=2 )
+        if(distance <=1 )
         {      
             parent.ChangeState(new IdleState());
+        }
+        if(parent.Target)
+        {
+            parent.ChangeState(new FollowState());
         }
     }
 }
