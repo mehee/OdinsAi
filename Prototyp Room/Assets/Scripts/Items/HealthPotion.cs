4954f63a22592be08f4b42143086647876dc76b5
Forve <tobias.forve@gmx.de>
2018-10-03 20:39:02 +0200

    save

--- Prototyp Room/Assets/Scripts/Items/HealthPotion.cs
+++ Prototyp Room/Assets/Scripts/Items/HealthPotion.cs
@@ -3,17 +3,17 @@
 [CreateAssetMenu(fileName = "HealthPotion", menuName = "Items/Potion", order = 1)]
 public class HealthPotion : Item, IUseable 
 {
-	//[SerializeField]private int health;
+	[SerializeField]private int health;
 
 	public void Use()
 	{
-		/*
+		
 		//just usable if player have lost Health
-		if(Player.MyInstance.MyHealth.MyCurrentValue < Player.MyInstance.MyHealth.MyMaxValue)
-		{
-			Remove();
-		//	Player.MyInstance.MyHealth.MyCurrentValue += health;
-		}
+		// if(Player.MyInstance.MyHealth.MyCurrentValue < Player.MyInstance.MyHealth.MyMaxValue)
+		// {
+		// 	Remove();
+		// //	Player.MyInstance.MyHealth.MyCurrentValue += health;
+		// }
 			
 	}
 
