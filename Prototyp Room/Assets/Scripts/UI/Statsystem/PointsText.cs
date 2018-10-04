cffc49d275014b81b65525c553bea722176c8dd0
Unknown <HeihoffJan@gmail.com>
2018-10-04 16:46:05 +0200

    progress

---
+++ Prototyp Room/Assets/Scripts/UI/Statsystem/PointsText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointsText : MonoBehaviour {
+
+	// Use this for initialization
+	Player player;
+	Text text;
+	void Start () 
+	{
+		player = GetComponentInParent<Player>();
+		text = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{	
+		text.text = "Points to Spend : " + player.PointsToSpend;
+		
+	}
+}
