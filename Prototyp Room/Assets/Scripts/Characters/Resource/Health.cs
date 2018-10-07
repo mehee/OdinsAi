814654cdb0591a0fbce00fd01a17c48708b56fd5
Unknown <HeihoffJan@gmail.com>
2018-10-07 19:47:16 +0200

    dmg tooltip beim reaper prototype prefab included

--- Prototyp Room/Assets/Scripts/Characters/Resource/Health.cs
+++ Prototyp Room/Assets/Scripts/Characters/Resource/Health.cs
@@ -1,15 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : Resource 
 {
     private bool dmgReceived = false;
 
+    public bool hasParticleEffect=false;
+
+    public bool hasDmgText=false;
+
+    Text text;
+
    
 
     void Start()
     {
+       if(hasDmgText)
+        {
+           Text[] texts = GetComponentsInChildren<Text>();
+           foreach(Text v in texts)
+           if(v.tag=="DmgText")
+            text = v;
+            Debug.Log(text.tag);
+        }
       
     }
     public bool DmgReceived
@@ -32,15 +47,26 @@ public class Health : Resource
     public override void Reduce(float amount)
     {
         DmgReceived = true;
+        if(hasParticleEffect)
+        {
         int randomNumber = Random.Range(0,10) % 3;       
         GetComponentsInChildren<ParticleSystem>()[randomNumber++].Play();
         GetComponentsInChildren<ParticleSystem>()[randomNumber++].Play();
         GetComponentsInChildren<ParticleSystem>()[randomNumber++].Play();
+        }
+
+        if(hasDmgText)
+        {
+            
+             text.text= amount.ToString();
+             text.CrossFadeAlpha(1.0f, 0.01f, false);
+                text.CrossFadeAlpha(0.0f, 1, false);
+        }
         Value -= amount;
         if(Value <= 0)
         {
-            GetComponent<Character>().Die();
-        }
+            GetComponent<Character>().Die(); 
     }
     
+}
 }
