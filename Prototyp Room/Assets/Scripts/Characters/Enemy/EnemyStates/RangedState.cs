6aade81482d60f1748c997096f49cddcd6770167
Unknown <wachterm89@gmail.com>
2018-10-12 14:31:15 +0200

    finalfinal cleanup mehee

--- Prototyp Room/Assets/Scripts/Characters/Enemy/EnemyStates/RangedState.cs
+++ Prototyp Room/Assets/Scripts/Characters/Enemy/EnemyStates/RangedState.cs
@@ -4,15 +4,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RangedState : IState {
-
-    //private EnemyBehaviour parent;
-
     private Vector2 direction;
     private EnemyAbility abilityInstance;
 
     public void Enter(EnemyBehaviour parent)
     {
-        //this.parent = parent;
         Vector2 direction = (parent.Target.position - parent.transform.position).normalized;
         //vars = parent.Vars;
         abilityInstance = parent.AbilityInstance;
